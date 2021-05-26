1a0b14745517b376c1ee80167cf009feb24d02df
rhythmgothwal13 <rhythmgothwal13@gmail.com>
2021-05-26 12:12:22 +0530

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus : string = 'onilne';
+  
+  constructor(){
+      this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getColor(){
+     return this.serverStatus === 'online' ? 'green' : 'red'
+  }
+
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'Up till now no new server!'
+  serverCreated = false; 
+  tempServerName = 'Rhythm-server';
+  serverName = '';
+  servers=['test1','test2'];
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
+
+  ngOnInit(): void {
+  }
+  onCreateServer(){
+      this.serverCreationStatus = "Yay! server created! name is " + this.serverName;
+      this.serverCreated = true;
+      this.serverName = this.tempServerName;
+      this.servers.push(this.serverName);
+  }
+  onUpdateServerName(event : Event){
+    this.serverName =  (<HTMLInputElement>event.target).value;
+  }
+}
